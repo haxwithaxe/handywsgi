c5e333661299801df25bcc37daa46f16392326d2
haxwithaxe <me@haxwithaxe.net>
2015-11-29 18:05:44 -0500

    addded template helper into Application class

--- handywsgi/application.py
+++ handywsgi/application.py
@@ -1,7 +1,6 @@
 
-from genshi.template import TemplateLoader
 
-from . import status, HTTP_REQUEST_METHODS
+from . import status, templator, HTTP_REQUEST_METHODS
 
 
 class Application:
@@ -9,11 +8,10 @@ class Application:
 
     def __init__(self, config):
         self.config = config
-        self.template_loader = TemplateLoader(
+        self.templator = templator.Templator(
                 self.config.template_path,
-                auto_reload=True
+                self.config.template_extension or 'html'
                 )
-        self.template_extension = config.template_extension or '.html'
         self.context = None
         self.content = None
 
@@ -28,25 +26,22 @@ class Application:
         else:
             raise status.NoMethod(self)
 
-    def format(self, template_name, **data):
+    def format(self, template_name=None, **data):
         """ Render a template with data and return it. """
-        content_template = self.template_loader.load(
-                template_name+self.template_extension
-                )
-        content = content_template.generate(**data).render(
-                'html',
-                doctype='html'
-                )
+        if not template_name:
+            template_name = self.config.default_template
+        content = self.templator.render(template_name, **data)
         return content
 
-    def render(self, template_name):
+    def render(self, template_name=None):
         """ Render content and send it immediately. """
-        page_template = self.template_loader.load(
-                template_name+self.template_extension
-                )
-        page = page_template.generate(app=self).render('html', doctype='html')
+        if not template_name:
+            template_name = self.config.default_template
+        template = self.templator.get_template(template_name)
+        page = template.render(app=self)
         self.context.response.output.write(page)
 
     def dump(self, data):
         """ Dump data directly to the output buffer """
         self.context.response.output.write(data)
+
